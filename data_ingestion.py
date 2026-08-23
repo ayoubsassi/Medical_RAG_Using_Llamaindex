cf4ecaa29b1233c946836a04980a81efc168f393
Ayoub <ayoubsassi51@gmail.com>
2026-08-23 16:32:32 -0400

    update ingest

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,25 +1,23 @@
-from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.ingestion import IngestionPipeline
-from rag_setup import get_embed_model, get_vector_store
+from utils import get_embed_model, get_vector_store, load_doc
 import config
 
-# shared setup (from rag_setup)
-embed_model = get_embed_model()
-client, vector_store = get_vector_store()
 
-# ingestion-specific
-documents = SimpleDirectoryReader(config.DATA_DIR).load_data()
+def main():
+    documents = load_doc()
+    embed_model = get_embed_model()
+    client, vector_store = get_vector_store()
+    pipeline = IngestionPipeline(
+        transformations=[
+            SentenceSplitter(chunk_size=config.CHUNK_SIZE, chunk_overlap=config.CHUNK_OVERLAP),
+            embed_model,
+        ],
+        vector_store=vector_store,
+    )
+    pipeline.run(documents=documents, show_progress=True)
+    print("Points stored in Qdrant:", client.get_collection(config.COLLECTION_NAME).points_count)
+    print("Ingestion complete.")
 
-pipeline = IngestionPipeline(
-    transformations=[
-        SentenceSplitter(chunk_size=config.CHUNK_SIZE, chunk_overlap=config.CHUNK_OVERLAP),
-        embed_model,
-    ],
-    vector_store=vector_store,
-)
-
-pipeline.run(documents=documents, show_progress=True)
-
-print("Points stored in Qdrant:", client.get_collection(config.COLLECTION_NAME).points_count)
-print("Ingestion complete.")
+if __name__ == "__main__":
+    main()
